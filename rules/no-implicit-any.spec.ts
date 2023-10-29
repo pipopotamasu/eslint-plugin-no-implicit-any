6f5897a1d9bbd5cd0f6622afa18bb82ecedad23a
pipopotamasu <ec0156hx@gmail.com>
2023-10-29 17:31:45 -0400

    Object params (#1)
    * property
    
    * object arrary params
    
    * add comment

--- rules/no-implicit-any.spec.ts
+++ rules/no-implicit-any.spec.ts
@@ -16,56 +16,128 @@ ruleTester.run(
     valid: [
       // FunctionDeclaration or FunctionExpression
       {
-        code: 'function foo (arg: any) {}',
+        code: 'function foo (arg1: any, arg2: any) {}',
       },
       {
-        code: 'const arrayObjFunc = [{ key: function (num: any) {} }];',
+        code: 'const arrayObjFunc = [{ key: function (arg1: any, arg2: any) {} }];',
+      },
+      {
+        code: 'function foo ({ arg1, arg2 }: any) {}',
+      },
+      {
+        code: 'function foo ({ ...rest }) {}',
+      },
+      {
+        code: 'function foo ({ ...rest }: any) {}',
+      },
+      {
+        code: 'function foo ({ arg1, ...rest }: any) {}',
+      },
+      {
+        code: 'function foo (...args: any[]) {}',
+      },
+      {
+        code: 'function foo (arg1: any, ...args: any[]) {}',
       },
       // ArrowFunctionExpression
       {
-        code: 'const foo = (arg: any) => {}',
+        code: 'const foo = (arg1: any, arg2: any) => {}',
       },
       {
-        code: 'const arrayObjArrowFunc = [{ key: (num: any) => num + 1 }];',
+        code: 'const arrayObjArrowFunc = [{ key: (arg1: any, arg2: any ) => {} }];',
+      },
+      {
+        code: 'const foo = ({ arg1, arg2 }: any) => {}',
+      },
+      {
+        code: 'const foo = ({ ...rest }) => {}',
+      },
+      {
+        code: 'const foo = ({ ...rest }: any) => {}',
+      },
+      {
+        code: 'const foo = ({ arg1, ...rest }: any) => {}',
+      },
+      {
+        code: 'const foo = (...args: any[]) => {}',
+      },
+      {
+        code: 'const foo = (arg1: any, ...args: any[]) => {}',
       },
       {
         code: `
-          type Foo = (firstArg: string, secondArg: string) => void
-          const foo: Foo = (firstArg, secondArg) => {};
+          type Type = (arg1: string, arg2: string) => void
+          const foo: Type = (...args) => {}
         `,
       },
-      // TODO: This should be passed.
+      {
+        code: `
+          type Foo = (arg1: string, arg2: string) => void
+          const foo: Foo = (arg1, arg2) => {};
+        `,
+      },
+      // TODO: These should be passed.
       // {
       //   code: `
       //     type Foo = (arg: any) => void
       //     const foo: Foo = (arg) => {};
       //   `,
       // },
+      // {
+      //   code: `
+      //     type Foo = (arg1: string, arg2: any[]) => void
+      //     const foo: Foo = (arg1, arg2) => {};
+      //   `,
+      // },
     ],
-    // 'invalid' checks cases that should not pass
     invalid: [
       // FunctionDeclaration or FunctionExpression
       {
-        code: 'function foo (firstArg, secondArg) {}',
-        output: 'function foo (firstArg: any, secondArg: any) {}',
+        code: 'function foo (arg1, arg2) {}',
+        output: 'function foo (arg1: any, arg2: any) {}',
         errors: [{ messageId: 'missingAnyType' }, { messageId: 'missingAnyType' }],
       },
       {
-        code: 'const arrayObjFunc = [{ key: function (num) {} }];',
-        output: 'const arrayObjFunc = [{ key: function (num: any) {} }];',
-        errors: [{ messageId: 'missingAnyType' }],
+        code: 'const arrayObjFunc = [{ key: function (arg1, arg2) {} }];',
+        output: 'const arrayObjFunc = [{ key: function (arg1: any, arg2: any) {} }];',
+        errors: [{ messageId: 'missingAnyType' }, { messageId: 'missingAnyType' }],
+      },
+      {
+        code: 'function foo ({ arg1, arg2 }) {}',
+        output: 'function foo ({ arg1, arg2 }: any) {}',
+        errors: [{ messageId: 'missingAnyType' }, { messageId: 'missingAnyType' }]
+      },
+      {
+        code: 'function foo ({ arg1, ...rest }) {}',
+        output: 'function foo ({ arg1, ...rest }: any) {}',
+        errors: [{ messageId: 'missingAnyType' }]
+      },
+      {
+        code: 'function foo (...args) {}',
+        output: 'function foo (...args: any[]) {}',
+        errors: [{ messageId: 'missingAnyType' }]
       },
       // ArrowFunctionExpression
       {
-        code: 'const foo = (arg) => {}',
-        output: 'const foo = (arg: any) => {}',
-        errors: [{ messageId: 'missingAnyType' }],
+        code: 'const foo = (arg1, arg2) => {}',
+        output: 'const foo = (arg1: any, arg2: any) => {}',
+        errors: [{ messageId: 'missingAnyType' }, { messageId: 'missingAnyType' }],
       },
       {
-        code: 'const arrayObjArrowFunc = [{ key: (num) => {} }];',
-        output: 'const arrayObjArrowFunc = [{ key: (num: any) => {} }];',
-        errors: [{ messageId: 'missingAnyType' }],
-      }
+        code: 'const arrayObjArrowFunc = [{ key: (arg1, arg2) => {} }];',
+        output: 'const arrayObjArrowFunc = [{ key: (arg1: any, arg2: any) => {} }];',
+        errors: [{ messageId: 'missingAnyType' }, { messageId: 'missingAnyType' }],
+      },
+      {
+        code: 'const foo = ({ arg1, arg2 }) => {}',
+        output: 'const foo = ({ arg1, arg2 }: any) => {}',
+        errors: [{ messageId: 'missingAnyType' }, { messageId: 'missingAnyType' }],
+      },
+      {
+        code: 'const foo = ({ arg1, ...rest }) => {}',
+        output: 'const foo = ({ arg1, ...rest }: any) => {}',
+        errors: [{ messageId: 'missingAnyType' }]
+      },
     ],
   }
 );
--- rules/no-implicit-any.ts
+++ rules/no-implicit-any.ts
@@ -1,5 +1,51 @@
-import { ESLintUtils } from '@typescript-eslint/utils';
+import { ESLintUtils, TSESLint } from '@typescript-eslint/utils';
+import { type TSESTree, AST_NODE_TYPES } from '@typescript-eslint/types';
+
 import * as ts from 'typescript';
+
+function checkArg (context: Readonly<TSESLint.RuleContext<'missingAnyType', any[]>>, arg: TSESTree.Parameter) {
+  if (!arg['typeAnnotation']) {
+    const parserServices = ESLintUtils.getParserServices(context);
+    const type = parserServices.getTypeAtLocation(arg);
+
+    if (type.flags === ts.TypeFlags.Any) {
+      context.report({
+        node: arg,
+        messageId: 'missingAnyType',
+        fix(fixer) {
+          return fixer.insertTextAfter(arg, ': any');
+        },
+      });
+    } else if (type.flags === ts.TypeFlags.Object) {
+      if (arg.type === AST_NODE_TYPES.ObjectPattern) {
+        arg.properties.forEach((property) => {
+          if (property.type === AST_NODE_TYPES.Property) {
+            if (!property.key['typeAnnotation']) {
+              const type = parserServices.getTypeAtLocation(property);
+              if (type.flags === ts.TypeFlags.Any) {
+                context.report({
+                  node: arg,
+                  messageId: 'missingAnyType',
+                  fix(fixer) {
+                    return fixer.insertTextAfter(arg, ': any');
+                  },
+                });
+              }
+            }
+          }
+        })
+      } else if (type.symbol?.escapedName === 'Array') {
+        context.report({
+          node: arg,
+          messageId: 'missingAnyType',
+          fix(fixer) {
+            return fixer.insertTextAfter(arg, ': any[]');
+          },
+        });
+      }
+    }
+  }
+}
 
 export const rule = ESLintUtils.RuleCreator.withoutDocs({
   defaultOptions: [],
@@ -15,57 +61,18 @@ export const rule = ESLintUtils.RuleCreator.withoutDocs({
   create: function (context) {
     return {
       FunctionDeclaration(node) {
-        node.params.forEach((param) => {
-          if (!param['typeAnnotation']) {
-            const parserServices = ESLintUtils.getParserServices(context);
-            const type = parserServices.getTypeAtLocation(param);
-
-            if (type.flags === ts.TypeFlags.Any) {
-              context.report({
-                node,
-                messageId: 'missingAnyType',
-                fix(fixer) {
-                  return fixer.insertTextAfter(param, ': any');
-                },
-              });
-            }
-          }
+        node.params.forEach((arg) => {
+          checkArg(context, arg);
         });
       },
       FunctionExpression(node) {
-        node.params.forEach((param) => {
-          if (!param['typeAnnotation']) {
-            const parserServices = ESLintUtils.getParserServices(context);
-            const type = parserServices.getTypeAtLocation(param);
-
-            if (type.flags === ts.TypeFlags.Any) {
-              context.report({
-                node,
-                messageId: 'missingAnyType',
-                fix(fixer) {
-                  return fixer.insertTextAfter(param, ': any');
-                },
-              });
-            }
-          }
+        node.params.forEach((arg) => {
+          checkArg(context, arg);
         });
       },
       ArrowFunctionExpression(node) {
-        node.params.forEach((param) => {
-          if (!param['typeAnnotation']) {
-            const parserServices = ESLintUtils.getParserServices(context);
-            const type = parserServices.getTypeAtLocation(param);
-
-            if (type.flags === ts.TypeFlags.Any) {
-              context.report({
-                node,
-                messageId: 'missingAnyType',
-                fix(fixer) {
-                  return fixer.insertTextAfter(param, ': any');
-                },
-              });
-            }
-          }
+        node.params.forEach((arg) => {
+          checkArg(context, arg);
         });
       },
     };
