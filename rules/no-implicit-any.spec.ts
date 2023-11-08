daf1c40d1caf4f320c12558165903799d04acafa
pipopotamasu <ec0156hx@gmail.com>
2023-11-07 21:24:04 -0500

    fix MemberExpression bugs (#9)

--- rules/no-implicit-any.spec.ts
+++ rules/no-implicit-any.spec.ts
@@ -103,6 +103,30 @@ ruleTester.run(
       },
       {
         code: `
+          const foo = {};
+          (foo as any)['key'];
+        `
+      },
+      {
+        code: `
+          const foo = { key: { key2: {} } };
+          (foo['key']['key2'] as any)['key3'];
+        `
+      },
+      {
+        code: `
+          const foo = {};
+          (foo as any)['key']['key2'];
+        `
+      },
+      {
+        code: `
+          const foo = {};
+          (foo as any).key1.key2;
+        `
+      },
+      {
+        code: `
           const foo = { key: 'value' };
           foo.key;
         `,
@@ -112,6 +136,42 @@ ruleTester.run(
           const foo = { key: 'value' };
           foo?.key;
         `,
+      },
+      {
+        code: `
+          const foo = {};
+          foo.key;
+        `,
+      },
+      {
+        code: `
+          const foo = { key: { key2: {} } };
+          foo.key.key2.key3;
+        `,
+      },
+      {
+        code: `
+          const foo = {};
+          foo?.key;
+        `,
+      },
+      {
+        code: `
+          const foo = { key:  { key2: {} } };
+          foo.key.key2?.key3;
+        `,
+      },
+      {
+        code: `
+          const foo: any[] = [];
+          foo[0];
+        `
+      },
+      {
+        code: `
+          const foo: { key: any } = { key: '' };
+          foo.key;
+        `
       },
       // VariableDeclarator
       {
@@ -227,50 +287,6 @@ ruleTester.run(
         output: `
           const foo = { key: { key2: {} } };
           (foo['key']['key2'] as any)?.['key3'];
-        `,
-        errors: [{ messageId: 'missingAnyType' }]
-      },
-      {
-        code: `
-          const foo = {};
-          foo.key;
-        `,
-        output: `
-          const foo = {};
-          (foo as any).key;
-        `,
-        errors: [{ messageId: 'missingAnyType' }]
-      },
-      {
-        code: `
-          const foo = { key: { key2: {} } };
-          foo.key.key2.key3;
-        `,
-        output: `
-          const foo = { key: { key2: {} } };
-          (foo.key.key2 as any).key3;
-        `,
-        errors: [{ messageId: 'missingAnyType' }]
-      },
-      {
-        code: `
-          const foo = {};
-          foo?.key;
-        `,
-        output: `
-          const foo = {};
-          (foo as any)?.key;
-        `,
-        errors: [{ messageId: 'missingAnyType' }]
-      },
-      {
-        code: `
-          const foo = { key:  { key2: {} } };
-          foo.key.key2?.key3;
-        `,
-        output: `
-          const foo = { key:  { key2: {} } };
-          (foo.key.key2 as any)?.key3;
         `,
         errors: [{ messageId: 'missingAnyType' }]
       },
