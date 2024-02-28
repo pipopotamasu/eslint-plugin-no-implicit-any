4942a219b2b009cff2edb5e69b54a569cd1f4c58
pipopotamasu <ec0156hx@gmail.com>
2024-02-27 21:40:49 -0500

    refactor isNullOrUndefinedOrVoid function

--- rules/helper.ts
+++ rules/helper.ts
@@ -1,15 +1,22 @@
 import { type TSESTree, AST_NODE_TYPES } from '@typescript-eslint/types';
 import type { CompilerOptions } from 'typescript';
 
-export function isNullOrUndefinedOrVoid(node: TSESTree.Expression) {
-  if (node.type === AST_NODE_TYPES.Literal) {
-    return node.value === null;
-  } else if (node.type === AST_NODE_TYPES.Identifier) {
-    return node.name === 'undefined';
-  } else if (node.type === AST_NODE_TYPES.UnaryExpression) {
-    return node.operator === 'void';
-  }
+function isNull(node: TSESTree.Expression) {
+  return node.type === AST_NODE_TYPES.Literal && node.value === null;
+}
 
+function isUndefined(node: TSESTree.Expression) {
+  return node.type === AST_NODE_TYPES.Identifier && node.name === 'undefined';
+}
+
+function isVoid(node: TSESTree.Expression) {
+  return node.type === AST_NODE_TYPES.UnaryExpression && node.operator === 'void';
+}
+
+export function isNullOrUndefinedOrVoid(node: TSESTree.Expression) {
+  if (isNull(node)) return true;
+  if (isUndefined(node)) return true;
+  if (isVoid(node)) return true;
   return false;
 }
 
