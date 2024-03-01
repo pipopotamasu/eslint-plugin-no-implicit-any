7e289414f75390b95a34290f6b8a079f953c31f0
pipopotamasu <ec0156hx@gmail.com>
2024-02-29 22:02:06 -0500

    add object expression lint

--- rules/no-implicit-any/object-expression/index.spec.ts
+++ rules/no-implicit-any/object-expression/index.spec.ts
@@ -3,25 +3,25 @@ import { rule } from '../../no-implicit-any';
 
 ruleTester.run('object-expression', rule, {
   valid: [
-    // { code: 'const foo = { a: 1 }' },
-    // { code: 'const foo = { a: null } as any' },
-    // { code: 'const foo: any = { a: null }' },
-    // { code: 'const foo = ({ ...rest }) => {}' },
+    { code: 'const foo = { a: 1 }' },
+    { code: 'const foo = { a: null } as any' },
+    { code: 'const foo: any = { a: null }' },
+    { code: 'const foo = ({ ...rest }) => {}' },
   ],
   invalid: [
-    // {
-    //   code: 'const foo = { a: null }',
-    //   output: 'const foo = { a: null as null }',
-    //   errors: [{ messageId: 'missingAnyType' }],
-    // },
-    // {
-    //   code: 'const foo = { a: undefined }',
-    //   output: 'const foo = { a: undefined as undefined }',
-    //   errors: [{ messageId: 'missingAnyType' }],
-    // },
+    {
+      code: 'const foo = { a: null }',
+      output: 'const foo = { a: null as null }',
+      errors: [{ messageId: 'missingAnyType' }],
+    },
+    {
+      code: 'const foo = { a: undefined }',
+      output: 'const foo = { a: undefined as undefined }',
+      errors: [{ messageId: 'missingAnyType' }],
+    },
     {
       code: 'const foo = { a: [] }',
-      output: 'const foo = { a: [] as any[] }',
+      output: 'const foo = { a: [] as never[] }',
       errors: [{ messageId: 'missingAnyType' }],
     },
   ],
--- rules/no-implicit-any/object-expression/index.ts
+++ rules/no-implicit-any/object-expression/index.ts
@@ -1,17 +1,19 @@
 import { ESLintUtils, type TSESLint } from '@typescript-eslint/utils';
 import { type TSESTree, AST_NODE_TYPES } from '@typescript-eslint/types';
-import { isNullOrUndefinedOrVoid, enabledStrictNullChecks, isNull, isUndefined } from '../../helper';
-
+import { enabledStrictNullChecks, isNull, isUndefined } from '../../helper';
 
 export const lintObjectExpression = (
   context: Readonly<TSESLint.RuleContext<'missingAnyType', any[]>>,
   node: TSESTree.ObjectExpression
 ) => {
+  const parserServices = ESLintUtils.getParserServices(context);
+  if (enabledStrictNullChecks(parserServices.program.getCompilerOptions())) return;
+
   const parent = node.parent;
   if (parent.type === AST_NODE_TYPES.TSAsExpression) return;
   if (parent.type === AST_NODE_TYPES.VariableDeclarator && parent.id.typeAnnotation) return;
 
-  node.properties.forEach(property => {
+  node.properties.forEach((property) => {
     if (property.type === AST_NODE_TYPES.SpreadElement) return;
 
     const value = property.value;
@@ -32,7 +34,14 @@ export const lintObjectExpression = (
           return fixer.insertTextAfter(value, ' as undefined');
         },
       });
+    } else if (value.type === AST_NODE_TYPES.ArrayExpression && value.elements.length === 0) {
+      context.report({
+        node: value,
+        messageId: 'missingAnyType',
+        fix(fixer) {
+          return fixer.insertTextAfter(value, ' as never[]');
+        },
+      });
     }
   });
 };
-
