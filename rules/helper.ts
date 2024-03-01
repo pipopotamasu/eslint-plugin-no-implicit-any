2d546cbb59f0d2bc68d9ef61760ed9cbe4e55fd7
pipopotamasu <ec0156hx@gmail.com>
2024-02-29 22:04:00 -0500

    Add object expression (#55)
    * wip
    
    * add object expression lint

--- rules/helper.ts
+++ rules/helper.ts
@@ -1,15 +1,15 @@
 import { type TSESTree, AST_NODE_TYPES } from '@typescript-eslint/types';
 import type { CompilerOptions } from 'typescript';
 
-export function isNull(node: TSESTree.Expression) {
+export function isNull(node: TSESTree.Node) {
   return node.type === AST_NODE_TYPES.Literal && node.value === null;
 }
 
-export function isUndefined(node: TSESTree.Expression) {
+export function isUndefined(node: TSESTree.Node) {
   return node.type === AST_NODE_TYPES.Identifier && node.name === 'undefined';
 }
 
-export function isVoid(node: TSESTree.Expression) {
+export function isVoid(node: TSESTree.Node) {
   return node.type === AST_NODE_TYPES.UnaryExpression && node.operator === 'void';
 }
 
---
+++ rules/no-implicit-any/object-expression/index.ts
@@ -0,0 +1,47 @@
+import { ESLintUtils, type TSESLint } from '@typescript-eslint/utils';
+import { type TSESTree, AST_NODE_TYPES } from '@typescript-eslint/types';
+import { enabledStrictNullChecks, isNull, isUndefined } from '../../helper';
+
+export const lintObjectExpression = (
+  context: Readonly<TSESLint.RuleContext<'missingAnyType', any[]>>,
+  node: TSESTree.ObjectExpression
+) => {
+  const parserServices = ESLintUtils.getParserServices(context);
+  if (enabledStrictNullChecks(parserServices.program.getCompilerOptions())) return;
+
+  const parent = node.parent;
+  if (parent.type === AST_NODE_TYPES.TSAsExpression) return;
+  if (parent.type === AST_NODE_TYPES.VariableDeclarator && parent.id.typeAnnotation) return;
+
+  node.properties.forEach((property) => {
+    if (property.type === AST_NODE_TYPES.SpreadElement) return;
+
+    const value = property.value;
+
+    if (isNull(value)) {
+      context.report({
+        node: value,
+        messageId: 'missingAnyType',
+        fix(fixer) {
+          return fixer.insertTextAfter(value, ' as null');
+        },
+      });
+    } else if (isUndefined(value)) {
+      context.report({
+        node: value,
+        messageId: 'missingAnyType',
+        fix(fixer) {
+          return fixer.insertTextAfter(value, ' as undefined');
+        },
+      });
+    } else if (value.type === AST_NODE_TYPES.ArrayExpression && value.elements.length === 0) {
+      context.report({
+        node: value,
+        messageId: 'missingAnyType',
+        fix(fixer) {
+          return fixer.insertTextAfter(value, ' as never[]');
+        },
+      });
+    }
+  });
+};
